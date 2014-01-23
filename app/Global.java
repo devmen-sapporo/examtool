7ba78097294fdd2ef21b7a73c1b91cf7c28dd327
a-suzuki <atsushisuzuki.mail@gmail.com>
2014-01-23 22:52:05 +0900

    起動時データ生成処理追加

--- app/Global.java
+++ app/Global.java
@@ -1,14 +1,36 @@
+import java.util.*;
+
+import models.entity.*;
 import play.*;
+import play.libs.*;
+
+import com.avaje.ebean.*;
 
 public class Global extends GlobalSettings {
 	
 	@Override
 	public void onStart(Application app) {
 		Logger.info("Application has started");
+        InitialData.insert(app);
 	}
 	
 	@Override
 	public void onStop(Application app) {
 	    Logger.info("Application shutdown...");
 	}
+	
+	static class InitialData {
+        
+        public static void insert(Application app) {
+            if(Ebean.find(User.class).findRowCount() == 0) {
+                
+                @SuppressWarnings("unchecked")
+								Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
+
+                Ebean.save(all.get("users"));
+                
+            }
+        }
+        
+    }
 }
